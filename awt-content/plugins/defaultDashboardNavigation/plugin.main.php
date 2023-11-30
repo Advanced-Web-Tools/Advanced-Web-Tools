77514e9021010697cf7ebd2ce61d70c9f47ac03f
ElStefanos <stefancrkvenjakov1@gmail.com>
2023-11-30 17:37:53 +0100

    Added store navigation item

--- awt-content/plugins/defaultDashboardNavigation/plugin.main.php
+++ awt-content/plugins/defaultDashboardNavigation/plugin.main.php
@@ -13,9 +13,9 @@ if (!defined('DASHBOARD') || !checkForPlugin('floatingEditor', '0.0.1') == true)
     $nav->addItem(array('icon' => $location . 'house-solid.svg', 'name' => 'Dashboard', 'link' => HOSTNAME . 'awt-admin/?page=Dashboard', 'permission' => 2));
     $nav->addItem(array('icon' => $location . 'file-lines-solid.svg', 'name' => 'Pages', 'link' => HOSTNAME . 'awt-admin/?page=Pages', 'permission' => 2));
     $nav->addItem(array('icon' => $location . 'brush-solid.svg', 'name' => 'Themes', 'link' => HOSTNAME . 'awt-admin/?page=Themes', 'permission' => 0));
-    $nav->addItem(array('icon' => $location . 'compass-solid.svg', 'name' => 'Menus', 'link' => HOSTNAME . 'awt-admin/?page=Menus', 'permission' => 1));
     $nav->addItem(array('icon' => $location . 'puzzle-piece-solid.svg', 'name' => 'Plugins', 'link' => HOSTNAME . 'awt-admin/?page=Plugins', 'permission' => 0));
-    //TO BE IMPLEMENTED $nav->addItem(array('icon' => $location . 'store-solid.svg', 'name' => 'Store', 'link' => HOSTNAME . 'awt-admin/?page=Store', 'permission' => 0));
+    $nav->addItem(array('icon' => $location . 'store-solid.svg', 'name' => 'Store', 'link' => HOSTNAME . 'awt-admin/?page=Store', 'permission' => 0));
+    $nav->addItem(array('icon' => $location . 'compass-solid.svg', 'name' => 'Menus', 'link' => HOSTNAME . 'awt-admin/?page=Menus', 'permission' => 1));
     $nav->addItem(array('icon' => $location . 'media.svg', 'name' => "My Media", 'link' => HOSTNAME . 'awt-admin/?page=Media', 'permission' => 2));
     $nav->addItem(array('icon' => $location . 'sliders-solid.svg', 'name' => "Theme Editor", 'link' => HOSTNAME . 'awt-admin/?page=ThemeEditor', 'permission' => 0, 'attr' => 'target="_blank"'));
     $nav->addItem(array('icon' => $location . 'users-solid.svg', 'name' => 'Accounts', 'link' => HOSTNAME . 'awt-admin/?page=Accounts', 'permission' => 2));
