4a0dc4cc0e7e3611b8b19fa5fe9490d22f56425b
ElStefanos <stefancrkvenjakov1@gmail.com>
2024-01-22 22:40:41 +0100

    Database file update

--- awt-database.sql
+++ awt-database.sql
@@ -1,22 +1,27 @@
-
+-- phpMyAdmin SQL Dump
+-- version 5.2.1deb1ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 22, 2024 at 09:38 PM
+-- Server version: 8.0.35-0ubuntu0.23.10.1
+-- PHP Version: 8.2.10-2ubuntu1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-
--- --------------------------------------------------------
-
---
--- Table structure for table `awt_access_authorization`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 CREATE TABLE IF NOT EXISTS `awt_access_authorization` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `fileName` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `fileHash` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `uniqueKey` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `fileName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `fileHash` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `uniqueKey` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -28,16 +33,16 @@ CREATE TABLE IF NOT EXISTS `awt_access_authorization` (
 
 CREATE TABLE IF NOT EXISTS `awt_admin` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `firstname` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `lastname` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `last_logged_ip` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `firstname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `lastname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `last_logged_ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `permission_level` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -47,22 +52,7 @@ CREATE TABLE IF NOT EXISTS `awt_admin` (
 
 CREATE TABLE IF NOT EXISTS `awt_albums` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `awt_cron`
---
-
-CREATE TABLE IF NOT EXISTS `awt_cron` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `interval` int NOT NULL,
-  `last_run` int NOT NULL,
-  `caller` varchar(255) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -74,10 +64,10 @@ CREATE TABLE IF NOT EXISTS `awt_cron` (
 
 CREATE TABLE IF NOT EXISTS `awt_mail` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `sender` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `recipient` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `subject` text COLLATE utf8mb4_general_ci NOT NULL,
-  `content` text COLLATE utf8mb4_general_ci NOT NULL,
+  `sender` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `recipient` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `subject` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `sent` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
@@ -92,9 +82,9 @@ CREATE TABLE IF NOT EXISTS `awt_mail` (
 CREATE TABLE IF NOT EXISTS `awt_media` (
   `id` int NOT NULL AUTO_INCREMENT,
   `album_id` int DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `file` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `file_type` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `file` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `file_type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -107,26 +97,18 @@ CREATE TABLE IF NOT EXISTS `awt_media` (
 
 CREATE TABLE IF NOT EXISTS `awt_menus` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `items` text COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `items` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `active` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `awt_metrics`
+-- Dumping data for table `awt_menus`
 --
 
-CREATE TABLE IF NOT EXISTS `awt_metrics` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `uid` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `url` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `ip` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=824 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT IGNORE INTO `awt_menus` (`id`, `name`, `items`, `active`) VALUES
+(1, 'Default Menu', '<a href=\'?page=Home\'>Home</a>NEW_LINK<a href=\'?page=Blog\'>Blog</a>NEW_LINK<a href=\'?page=About Us\'>About Us</a>NEW_LINK', 1);
 
 -- --------------------------------------------------------
 
@@ -136,9 +118,9 @@ CREATE TABLE IF NOT EXISTS `awt_metrics` (
 
 CREATE TABLE IF NOT EXISTS `awt_notifications` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `caller` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `content` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `importance` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `caller` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `importance` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=456 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -151,11 +133,12 @@ CREATE TABLE IF NOT EXISTS `awt_notifications` (
 
 CREATE TABLE IF NOT EXISTS `awt_paging` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `content_1` mediumtext COLLATE utf8mb4_general_ci,
-  `content_2` mediumtext COLLATE utf8mb4_general_ci,
-  `status` varchar(7) COLLATE utf8mb4_general_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `content_1` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `content_2` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `status` varchar(7) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `override` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -183,10 +166,10 @@ CREATE TABLE IF NOT EXISTS `awt_password_reset` (
 
 CREATE TABLE IF NOT EXISTS `awt_plugins` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `icon` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `version` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `description` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `version` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `status` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -199,8 +182,8 @@ CREATE TABLE IF NOT EXISTS `awt_plugins` (
 
 CREATE TABLE IF NOT EXISTS `awt_settings` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `value` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `value` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `required_permission_level` int NOT NULL DEFAULT '0',
   `category` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT 'Miscellaneous',
   PRIMARY KEY (`id`)
@@ -217,11 +200,10 @@ INSERT IGNORE INTO `awt_settings` (`id`, `name`, `value`, `required_permission_l
 (4, 'whitelist', 'false', 0, 'Security'),
 (5, 'whitelist_list', '127.0.0.1 ::1 localhost', 0, 'Security'),
 (6, 'use_plugins', 'true', 0, 'General'),
-(7, 'hostname_path', '/', 0, 'General'), -- Fixed the opening parenthesis here
+(7, 'hostname_path', '/testing/', 0, 'General'),
 (10, 'Enable API', 'true', 0, 'Security'),
 (11, 'API request whitelist', '*', 0, 'Security'),
 (13, 'PHP Error reporting', '1', 1, 'Security');
-
 
 -- --------------------------------------------------------
 
@@ -231,10 +213,10 @@ INSERT IGNORE INTO `awt_settings` (`id`, `name`, `value`, `required_permission_l
 
 CREATE TABLE IF NOT EXISTS `awt_themes` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `description` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `version` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `placeholder` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `version` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `placeholder` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `active` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -243,12 +225,9 @@ CREATE TABLE IF NOT EXISTS `awt_themes` (
 -- Dumping data for table `awt_themes`
 --
 
-INSERT IGNORE INTO `awt_themes` (`id`, `name`, `description`, `version`, `placeholder`, `active`)
-VALUES (1, 'Twenty-Twenty-Three', 'This is a sleek and modern theme for your website', '0.0.1', 'placeholder.png', 1);
+INSERT IGNORE INTO `awt_themes` (`id`, `name`, `description`, `version`, `placeholder`, `active`) VALUES
+(1, 'Twenty-Twenty-Three', 'This is a sleek and modern theme for your website', '0.0.1', 'placeholder.png', 1);
 
-
-INSERT IGNORE INTO `awt_menus` (`id`, `name`, `items`, `active`) VALUES
-(1, 'Default Menu', '<a href=\'?page=Home\'>Home</a>NEW_LINK<a href=\'?page=Blog\'>Blog</a>NEW_LINK<a href=\'?page=About Us\'>About Us</a>NEW_LINK', 1);
 -- --------------------------------------------------------
 
 --
@@ -258,8 +237,8 @@ INSERT IGNORE INTO `awt_menus` (`id`, `name`, `items`, `active`) VALUES
 CREATE TABLE IF NOT EXISTS `awt_theme_page` (
   `id` int NOT NULL AUTO_INCREMENT,
   `theme_id` int NOT NULL,
-  `page_name` varchar(255) NOT NULL,
-  `content` longtext NOT NULL,
+  `page_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `content` longtext COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -272,17 +251,14 @@ CREATE TABLE IF NOT EXISTS `awt_theme_page` (
 CREATE TABLE IF NOT EXISTS `awt_theme_settings` (
   `id` int NOT NULL AUTO_INCREMENT,
   `theme_id` int NOT NULL,
-  `category` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `value` varchar(255) NOT NULL,
+  `category` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `type` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `value` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
-START TRANSACTION;
-
-ALTER TABLE `awt_paging` ADD `description` VARCHAR(255) DEFAULT NULL AFTER `name`; 
-
-
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
