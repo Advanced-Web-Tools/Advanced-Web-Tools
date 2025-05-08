8395a6e5ea43f309797a78bcdfb0fde354a0a9c2
Stefan Crkvenjakov <stefancrkvenjakov1@gmail.com>
2025-05-08 13:55:57 +0200

    A

--- awt_db.php
+++ awt_db.php
@@ -1,9 +1,9 @@
 <?php
 
-const DB_HOSTNAME = "localhost";
-const DB_USERNAME = "root";
+const DB_HOSTNAME = "";
+const DB_USERNAME = "";
 const DB_PASSWORD = "";
-const DB_NAME = "awt_development";
-const DB_TYPE = "mysql";
+const DB_NAME = "";
+const DB_TYPE = "";
 
 
